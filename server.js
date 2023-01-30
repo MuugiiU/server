7fdaaedaa50d2938923cfdfdd7646bd5b1108c57
Evening <evening@admins-iMac-2.local>
2023-01-30 18:16:43 +0800

    file read

--- server.js
+++ server.js
@@ -1,8 +1,51 @@
 const express = require("express");
+const cors = require("cors");
+const fs = require("fs")
+
+               const port =8009;
 const server=express();
+server.use (cors());
+server.use(express.json());
 server.get("/",(req,res)=>{
     res.send("huseltend amjilttai");
 });
-server.listen(8008,()=>{
+server.post ("/signup", (req, res) =>{
+    const {name, role} =req.body;
+    fs.readFile("users.json", "utf-8", (err,data)=>{
+        if(err){
+            console.log("file unshihad aldaa garlaa !!!");
+            return;
+        }
+        console.log(data);
+        const parsedData = JSON.parse(data);
+        const newUser ={ name, role, id:parsedData.users.length}
+        parsedData.users.push(newUser);
+        fs.writeFile("users.json", JSON.stringify(parsedData),(err)=>{
+            if(err) {
+                res.status(400).json({message:"Error"})
+            }
+            res.status(201).json({message:"Created new user"})
+        })
+    })
+
+})
+server.get("/users",(req,res)=>{
+    fs.readFile("users.json", "utf-8",(err,data)=>{
+        if(err) {
+            console.log("Error")
+            return;
+        }
+        console.log(data);
+        const parsedData =JSON.parse(data);
+        
+        res.status(201).json({users: parsedData.users})
+    })
+})
+server.get("/users/:id", (req,res)=>{
+    const {id} =req.params;
+    const user = users.find((el)=>el.id===id);
+    res.status(200).json({user})
+})
+server.listen(port,()=>{
     console.log("server aslaa");
 });
